15e4b1310881d5f678fc15fa1781998b70c0a211
glycogen94 <20133132@kookmin.ac.kr>
2020-04-13 17:18:31 +0900

    sha256 버전

--- libsnark/zk_proof_systems/ppzksnark/snark_for_filtering/tests/test_snark_for_filtering.cpp
+++ libsnark/zk_proof_systems/ppzksnark/snark_for_filtering/tests/test_snark_for_filtering.cpp
@@ -22,31 +22,32 @@
 #include <opencv2/opencv.hpp>
 #include <string.h>
 #include <openssl/sha.h>
+#include <cmath>
 
 using namespace libsnark;
 using namespace cv;
 
 bool mouse_is_pressing = false;
-int start_x = -1, start_y = -1;
-int end_x = -1, end_y = -1;
 Mat original_array = cv::imread("/home/itsp/snark_for_filtering/libsnark/zk_proof_systems/ppzksnark/snark_for_filtering/tests/test.png",IMREAD_COLOR);
-Mat u1_array;
-Mat u2_array = original_array.clone();
-Mat img_show = original_array.clone();
-vector<unsigned char> u1_digest_vector[SHA256_DIGEST_LENGTH];
-vector<unsigned char> u2_digest_vector[SHA256_DIGEST_LENGTH];
-int stride=1;
 
+struct Image_ROI{
+    int start_x;
+    int start_y;
+    int end_x;
+    int end_y;
+};
 
-void mouse_callback(int event, int x, int y, int flags, void *userdata)
+template<typename ppT>
+void mouse_callback(int event, int x, int y, int flags, void *pt)
 {
 	Mat img_result = original_array.clone();
+    Image_ROI* ROI = (Image_ROI *) pt;
 
 	if (event == EVENT_LBUTTONDOWN){
 
 		mouse_is_pressing = true;
-		start_x = x;
-		start_y = y;
+		ROI->start_x = x;
+		ROI->start_y = y;
 
 		circle(img_result, Point(x, y), 10, Scalar(0, 255, 0), -1);
 		imshow("original", img_result);
@@ -55,80 +56,35 @@ void mouse_callback(int event, int x, int y, int flags, void *userdata)
 
 		if (mouse_is_pressing){
 			
-			rectangle(img_result, Point(start_x, start_y), Point(x, y),
+			rectangle(img_result, Point(ROI->start_x, ROI->start_y), Point(x, y),
 				Scalar(0, 255, 0), 1);
 
 			imshow("original", img_result);
-            end_x = x;
-            end_y = y;
+            ROI->end_x = x;
+            ROI->end_y = y;
 		}
 
 	} else if (event == EVENT_LBUTTONUP){
 
 		mouse_is_pressing = false;
-        if(start_x>end_x) std::swap(start_x, end_x);
-        if(start_y>end_y) std::swap(start_y, end_y);
-        // std::cout << "start_x: " << start_x << std::endl;
-        // std::cout << "end_x: " << end_x << std::endl;
-        // std::cout << "start_y: " << start_y << std::endl;
-        // std::cout << "end_y: " << end_y << std::endl;
+        if(ROI->start_x > ROI->end_x) std::swap(ROI->start_x, ROI->end_x);
+        if(ROI->start_y > ROI->end_y) std::swap(ROI->start_y, ROI->end_y);
 
-        start_x = start_x - start_x%stride;
-        start_y = start_y - start_y%stride;
-        end_x = ((end_x+stride-1)/stride)*stride;
-        end_y = ((end_y+stride-1)/stride)*stride;
-
-        // std::cout << "start_x: " << start_x << std::endl;
-        // std::cout << "end_x: " << end_x << std::endl;
-        // std::cout << "start_y: " << start_y << std::endl;
-        // std::cout << "end_y: " << end_y << std::endl;
-        
-        for (int i=start_y; i<end_y; i++){
-            uchar* ptr = u2_array.ptr<uchar>(i);
-            for (int j=start_x; j<end_x; j++){
-                ptr[j*3] = 0;
-                ptr[j*3+1] = 0;
-                ptr[j*3+2] = 0;
-            }
-        }
-        bitwise_xor(img_result, u2_array, u1_array);
-		imshow("original", img_result);
-		imshow("u2", u2_array);
-        imshow("u1", u1_array);
-    // {
-    // 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-    // 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-    // 0,0,255,0,0,0,0,0,0,255,255,255,0,0,0,0,
-    // 0,0,255,0,0,0,0,0,255,0,0,0,255,0,0,0,
-    // 0,0,255,0,0,0,0,255,0,0,0,0,0,255,0,0,
-    // 0,0,255,0,0,0,0,0,0,0,0,0,0,255,0,0,
-    // 0,0,255,0,0,0,0,0,0,0,0,0,0,255,0,0,
-    // 0,0,255,0,0,0,0,0,0,0,0,0,255,0,0,0,
-    // 0,0,255,0,0,0,0,0,0,0,0,255,0,0,0,0,
-    // 0,0,255,0,0,0,0,0,0,0,255,0,0,0,0,0,
-    // 0,0,255,0,0,0,0,0,0,255,0,0,0,0,0,0,
-    // 0,0,255,0,0,0,0,0,255,0,0,0,0,0,0,0,
-    // 0,0,255,0,0,0,0,255,0,0,0,0,0,0,0,0,
-    // 0,0,255,0,0,0,255,255,255,255,255,255,255,255,0,0,
-    // 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-    // 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-    // };
-        // for (int i=0; i<original_array.rows/stride; i++){
-        //     uchar* ptr_u1 = u1_array.ptr<uchar>(i*stride);
-        //     uchar* ptr_u2 = u2_array.ptr<uchar>(i*stride);
-        //     for (int j=0; j<original_array.cols/stride; j++){
-        //         unsigned char u1_digest[SHA256_DIGEST_LENGTH];
-        //         unsigned char u2_digest[SHA256_DIGEST_LENGTH];
-        //        SHA256((unsigned char*)&u1_array.at<uchar>(), strlen(u1_array), (unsigned char*)&u1_digest);
-        //        SHA256((unsigned char*)&u2_array.at<uchar>(), strlen(u2_array), (unsigned char*)&u2_digest);
-        //     }
-        // }
 	}
 }
 
 template<typename ppT>
 void test_snark_for_filtering()
 {   
+    Image_ROI pt;
+    Mat u1_array;
+    Mat u2_array = original_array.clone();
+    Mat img_show = original_array.clone();
+    int stride=1;
+
+    std::vector<libff::Fr<ppT>> u1;
+    std::vector<libff::Fr<ppT>> u2;
+    std::vector<libff::Fr<ppT>> original;
     std::cout<<"stride? ";
     std::cin>>stride;
     
@@ -140,105 +96,72 @@ void test_snark_for_filtering()
         }
     }
 
-    // char string[] = "hello world";
-
-    
-
-    // SHA256((unsigned char*)&string, strlen(string), (unsigned char*)&digest);    
-
-    // int original_array[]= {
-    // 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-    // 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-    // 0,0,255,0,0,0,0,0,0,255,255,255,0,0,0,0,
-    // 0,0,255,0,0,0,0,0,255,0,0,0,255,0,0,0,
-    // 0,0,255,0,0,0,0,255,0,0,0,0,0,255,0,0,
-    // 0,0,255,0,0,0,0,0,0,0,0,0,0,255,0,0,
-    // 0,0,255,0,0,0,0,0,0,0,0,0,0,255,0,0,
-    // 0,0,255,0,0,0,0,0,0,0,0,0,255,0,0,0,
-    // 0,0,255,0,0,0,0,0,0,0,0,255,0,0,0,0,
-    // 0,0,255,0,0,0,0,0,0,0,255,0,0,0,0,0,
-    // 0,0,255,0,0,0,0,0,0,255,0,0,0,0,0,0,
-    // 0,0,255,0,0,0,0,0,255,0,0,0,0,0,0,0,
-    // 0,0,255,0,0,0,0,255,0,0,0,0,0,0,0,0,
-    // 0,0,255,0,0,0,255,255,255,255,255,255,255,255,0,0,
-    // 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-    // 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-    // };
-    // int u1_array[]= {
-    // 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-    // 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-    // 0,0,255,0,0,0,0,0,0,0,0,0,0,0,0,0,
-    // 0,0,255,0,0,0,0,0,0,0,0,0,0,0,0,0,
-    // 0,0,255,0,0,0,0,0,0,0,0,0,0,0,0,0,
-    // 0,0,255,0,0,0,0,0,0,0,0,0,0,0,0,0,
-    // 0,0,255,0,0,0,0,0,0,0,0,0,0,0,0,0,
-    // 0,0,255,0,0,0,0,0,0,0,0,0,0,0,0,0,
-    // 0,0,255,0,0,0,0,0,0,0,0,0,0,0,0,0,
-    // 0,0,255,0,0,0,0,0,0,0,0,0,0,0,0,0,
-    // 0,0,255,0,0,0,0,0,0,0,0,0,0,0,0,0,
-    // 0,0,255,0,0,0,0,0,0,0,0,0,0,0,0,0,
-    // 0,0,255,0,0,0,0,0,0,0,0,0,0,0,0,0,
-    // 0,0,255,0,0,0,0,0,0,0,0,0,0,0,0,0,
-    // 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-    // 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-    // };
-    // int u2_array[]= {
-    // 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-    // 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-    // 0,0,0,0,0,0,0,0,0,255,255,255,0,0,0,0,
-    // 0,0,0,0,0,0,0,0,255,0,0,0,255,0,0,0,
-    // 0,0,0,0,0,0,0,255,0,0,0,0,0,255,0,0,
-    // 0,0,0,0,0,0,0,0,0,0,0,0,0,255,0,0,
-    // 0,0,0,0,0,0,0,0,0,0,0,0,0,255,0,0,
-    // 0,0,0,0,0,0,0,0,0,0,0,0,255,0,0,0,
-    // 0,0,0,0,0,0,0,0,0,0,0,255,0,0,0,0,
-    // 0,0,0,0,0,0,0,0,0,0,255,0,0,0,0,0,
-    // 0,0,0,0,0,0,0,0,0,255,0,0,0,0,0,0,
-    // 0,0,0,0,0,0,0,0,255,0,0,0,0,0,0,0,
-    // 0,0,0,0,0,0,0,255,0,0,0,0,0,0,0,0,
-    // 0,0,0,0,0,0,255,255,255,255,255,255,255,255,0,0,
-    // 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-    // 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-    // };
     imshow("original", original_array);
     imshow("stride", img_show);
-	setMouseCallback("original", mouse_callback);
-
+	setMouseCallback("original", mouse_callback<ppT>, (void *)&pt);
+    cv::waitKey(0);
+    
+    pt.start_x = pt.start_x - pt.start_x%stride;
+    pt.start_y = pt.start_y - pt.start_y%stride;
+    pt.end_x = ((pt.end_x+stride-1)/stride)*stride;
+    pt.end_y = ((pt.end_y+stride-1)/stride)*stride;
+    
+    for (int i=pt.start_y; i<pt.end_y; i++){
+        uchar* ptr = u2_array.ptr<uchar>(i);
+        for (int j=pt.start_x; j<pt.end_x; j++){
+            ptr[j*3] = 0;
+            ptr[j*3+1] = 0;
+            ptr[j*3+2] = 0;
+        }
+    }
+    bitwise_xor(original_array, u2_array, u1_array);
+	imshow("original", original_array);
+	imshow("u2", u2_array);
+    imshow("u1", u1_array);
+    int stride_rows = (int)ceil((double)original_array.rows/stride);
+    int stride_cols = (int)ceil((double)original_array.cols/stride);
+    Mat resize_original_array = Mat::zeros(stride_rows*stride, stride_cols*stride, CV_8UC3);
+    for(int i=0; i<original_array.rows; i++){
+        for(int j=0; j<original_array.cols; j++){
+            resize_original_array.at<Vec3b>(i,j)[0] = original_array.at<Vec3b>(i,j)[0];
+            resize_original_array.at<Vec3b>(i,j)[1] = original_array.at<Vec3b>(i,j)[1];
+            resize_original_array.at<Vec3b>(i,j)[2] = original_array.at<Vec3b>(i,j)[2];
+        }
+    }
+    imshow("resize_original_array", resize_original_array);
     cv::waitKey(0);
 	destroyAllWindows();
-
-    std::vector<libff::Fr<ppT>> u1;
-    std::vector<libff::Fr<ppT>> u2;
-    std::vector<libff::Fr<ppT>> original;
-
-    size_t img_size = original_array.rows * original_array.cols * 3;
-
-    // for(size_t i=0;i<256;i++){
-    //     u1.push_back(libff::Fr<ppT>(u1_array[i]));
-    // }
-    // for(size_t i=0;i<256;i++){
-    //     u2.push_back(libff::Fr<ppT>(u2_array[i]));
-    // }
-    // for(size_t i=0;i<256;i++){
-    //     original.push_back(libff::Fr<ppT>(original_array[i]));
-
-    // }
-    // }
-    // for(size_t i=0;i<256;i++){
-    //     u2.push_back(libff::Fr<ppT>(u2_array[i]));
-    // }
-    // for(size_t i=0;i<256;i++){
-    //     original.push_back(libff::Fr<ppT>(original_array[i]));
-    // }
-    // }
-
-    for(size_t i=0;i<img_size;i++){
-        u1.push_back(libff::Fr<ppT>(u1_array.data[i]));
-    
-        u2.push_back(libff::Fr<ppT>(u2_array.data[i]));
-    
-        original.push_back(libff::Fr<ppT>(original_array.data[i]));
+    for (int i=0; i<stride_rows; i++){
+        for (int j=0; j<stride_cols; j++){
+            Mat temp = resize_original_array(Rect(j*stride, i*stride, stride, stride));
+            unsigned char digest[SHA256_DIGEST_LENGTH];
+            SHA256((unsigned char*)&temp.data, stride*stride, (unsigned char*)&digest);
+            libff::Fr<ppT> sha_value = libff::Fr<ppT>::zero();
+            for (int k=0; k<SHA256_DIGEST_LENGTH; k++){
+                libff::Fr<ppT> tmp = sha_value * 256;
+                sha_value = tmp + digest[k];
+            }
+            if(pt.start_y/stride<= i && i <=pt.end_y/stride && pt.start_x/stride <= j && j <= pt.end_x/stride){
+                u1.push_back(sha_value);
+                u2.push_back(libff::Fr<ppT>::zero());
+            }
+            else{
+                u1.push_back(libff::Fr<ppT>::zero());
+                u2.push_back(sha_value);
+            }
+            original.push_back(sha_value);
+        }
     }
+
+    // size_t img_size = original_array.rows * original_array.cols * 3;
+
+    // for(size_t i=0;i<img_size;i++){
+    //     u1.push_back(libff::Fr<ppT>(u1_array.data[i]));
+    
+    //     u2.push_back(libff::Fr<ppT>(u2_array.data[i]));
+    
+    //     original.push_back(libff::Fr<ppT>(original_array.data[i]));
+    // }
 
     libff::print_header("(enter) Test Snark for Filtering");
     
